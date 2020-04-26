dcce47d7386c4afc87372f066292466eb8688de9
go je <27877342+gocs@users.noreply.github.com>
2020-04-26 21:34:11 +0800

    step 3

--- main.go
+++ main.go
@@ -15,6 +15,16 @@ func main() {
 	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
+	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
 	// Our application will run on port 3000. Here we declare the port and pass in our router.
 	http.ListenAndServe(":3000", r)
 }
+
+// Here we are implementing the NotImplemented handler. Whenever an API endpoint is hit
+// we will simply return the message "Not Implemented"
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
