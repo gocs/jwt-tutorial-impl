dc08bc861f870c2b302effcfcda1d130c0208fc7
go je <27877342+gocs@users.noreply.github.com>
2020-04-26 22:12:17 +0800

    step 18

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -53,6 +53,20 @@ var App = React.createClass({
 });
 
 var Home = React.createClass({
+    // Clicking the login link will redirect the user to the Hosted Lock page 
+    // where the user will provide their credentials and will then be redirected
+    // back to the application
+    authenticate: function () {
+        this.webAuth = new auth0.WebAuth({
+            domain: AUTH0_DOMAIN,
+            clientID: AUTH0_CLIENT_ID,
+            scope: 'openid profile',
+            audience: AUTH0_API_AUDIENCE,
+            responseType: 'token id_token',
+            redirectUri: AUTH0_CALLBACK_URL
+        });
+        this.webAuth.authorize();
+    },
     render: function () {
         return (
             <div className="container">
@@ -60,6 +74,7 @@ var Home = React.createClass({
                     <h1>We R VR</h1>
                     <p>Provide valuable feedback to VR experience developers.</p>
                     <a className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+                    <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
                 </div>
             </div>);
     }
