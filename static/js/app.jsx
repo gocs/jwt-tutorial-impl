09e98d19eab838dfd831d91f324923857f48f160
go je <27877342+gocs@users.noreply.github.com>
2020-04-26 21:46:39 +0800

    step 11

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -10,3 +10,16 @@ var App = React.createClass({
         }
     }
 });
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <div className="container">
+                <div className="col-xs-12 jumbotron text-center">
+                    <h1>We R VR</h1>
+                    <p>Provide valuable feedback to VR experience developers.</p>
+                    <a className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+                </div>
+            </div>);
+    }
+});
