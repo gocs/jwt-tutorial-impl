31b2631357f4279e975c3757e8dfe55938f03cfb
go je <27877342+gocs@users.noreply.github.com>
2020-04-28 01:50:19 +0800

    remove duplicate button in favor to button with click handler

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -73,7 +73,6 @@ var Home = React.createClass({
                 <div className="col-xs-12 jumbotron text-center">
                     <h1>We R VR</h1>
                     <p>Provide valuable feedback to VR experience developers.</p>
-                    <a className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
                     <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
                 </div>
             </div>);
